7df6b53a4655af1c83a844c2ba867d05554b01f6
Robin Boot <sercan.gulmus@gmail.com>
2022-06-02 09:20:03 +0300

    Add files via upload

---
+++ PasswordGenerator.ps1
@@ -0,0 +1,3 @@
+﻿$Password = New-Object -TypeName PSObject
+$Password | Add-Member -MemberType ScriptProperty -Name "Password" -Value { ("!@#$%^&*0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz".tochararray() | sort {Get-Random})[0..15] -join '' }
+$Password
